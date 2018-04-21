c8e311599da6ec0208b914452050bb94a29f51d5
James Lemaire <lythonikjames@gmail.com>
2018-04-21 17:31:53 -0400

    adding new apps for online Android Repo

---
+++ BoardingPass/app/src/main/java/com/nevictus/android/boardingpass/KLMActivity.java
@@ -0,0 +1,13 @@
+package com.nevictus.android.boardingpass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KLMActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_klm);
+    }
+}
